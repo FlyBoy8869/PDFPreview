c67d7e7e23f1c1d23d3b81d8c0438ab9571c95d5
Charles Cognato <charlescognato@gmail.com>
2026-02-26 19:00:39 -0500

    Clean-up.

--- PDFPreview/database/db.py
+++ PDFPreview/database/db.py
@@ -3,6 +3,6 @@ from pathlib import Path
 from tinydb import TinyDB
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-DB_PATH = BASE_DIR / "Resources/Files/bookmarks.json"
+DB_PATH = BASE_DIR / "Resources/Files/database.json"
 
 db = TinyDB(DB_PATH, indent=4)
