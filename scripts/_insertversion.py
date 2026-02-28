baeb8d1e9e7e60a601bd893bebc8e1ca92b432d1
Charles Cognato <charlescognato@gmail.com>
2026-02-28 07:25:19 -0500

    Adds a script to update the app version in __version__.py.

---
+++ scripts/_insertversion.py
@@ -0,0 +1,17 @@
+"""Extracts the version line from pyproject.toml and inserts it into __version__.py"""
+
+import sys
+from pathlib import Path
+import re
+
+regx = r'version = "(\d*\.\d*\.\d*).+"?'
+
+with Path("pyproject.toml").resolve().open(mode="r", encoding="utf-8") as infile:
+    for line in infile:
+        if match := re.search(regx, line):
+            print(line.upper())
+
+            with (Path(__file__).parent.parent / "__version__.py").resolve().open(mode="w", encoding="utf-8") as outfile:
+                outfile.write(line.upper())
+
+            sys.exit()
---
+++ scripts/insertversion.bat
@@ -0,0 +1,1 @@
+uv.exe run \scripts\_insertversion.py
