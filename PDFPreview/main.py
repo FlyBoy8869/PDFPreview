a32fb17c756da7a08ce7155469545ed940a3586b
Charles Cognato <charlescognato@gmail.com>
2026-03-01 19:08:30 -0500

    Adds a cleanup function to close the database.

--- PDFPreview/main.py
+++ PDFPreview/main.py
@@ -3,10 +3,15 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from .gui import MainWindow
+from .database.db import db
 
+def cleanup():
+    print("closing database")
+    db.close()
 
 def main() -> int:
     app = QApplication(sys.argv)
+    app.aboutToQuit.connect(cleanup)
     app.setStyle("Fusion")
     window = MainWindow()
     window.show()
