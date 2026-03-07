9ca7fe061453dcaa39bbb39da3c37fec77a513c6
Charles Cognato <charlescognato@gmail.com>
2026-03-07 12:28:04 -0500

    Updates script to use new version of app in command.

--- scripts/_copyfiles.bat
+++ scripts/_copyfiles.bat
@@ -1,4 +1,7 @@
 @echo off
 
+SET /p version_string=<__version__.py
+SET VERSION=%version_string:~11,6%
+
 echo Copying additional files...
-COPY .\CHANGELOG.md C:\Users\charles.cognato\PyApps\FileViewer
+COPY .\CHANGELOG.md C:\Users\charles.cognato\PyApps\FileViewer-%VERSION%
