4a6c5ea9ee9e8140cb0858f67da1f5501dfa7a8e
Charles Cognato <charlescognato@gmail.com>
2026-03-07 22:23:18 -0500

    Removes unnecessary clause.

--- scripts/_build.bat
+++ scripts/_build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 echo Building application with pyinstaller...
-C:\Users\charles.cognato\AppData\Roaming\Python\Scripts\uv.exe run pyinstaller --add-data PDFPreview\gui\dialogs\ui_about.ui:PDFPreview\gui\dialogs\ --add-data Resources:.\Resources --add-data .\CHANGELOG.md:. --upx-dir C:\Users\charles.cognato\upx-5.1.0-win64\upx-5.1.0-win64 --onedir --windowed --noconfirm --name FileViewer app.py
+C:\Users\charles.cognato\AppData\Roaming\Python\Scripts\uv.exe run pyinstaller --add-data PDFPreview\gui\dialogs\ui_about.ui:PDFPreview\gui\dialogs\ --add-data Resources:.\Resources --upx-dir C:\Users\charles.cognato\upx-5.1.0-win64\upx-5.1.0-win64 --onedir --windowed --noconfirm --name FileViewer app.py
 
 echo ... End _build.bat
