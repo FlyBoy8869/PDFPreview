eb0714e917ff35c042b68eb470b1e48cccef8593
Charles Cognato <charlescognato@gmail.com>
2026-03-17 19:16:15 -0400

    change: moves the database.json and config.toml files into the conventional operating system locations.

--- PDFPreview/database/db.py
+++ PDFPreview/database/db.py
@@ -1,12 +1,5 @@
-import os
-from pathlib import Path
-
 from tinydb import TinyDB
 
-_path = Path(os.path.expandvars(Path("%APPDATA%/FileViewer")))
-if not _path.exists():
-    _path.mkdir(parents=True)
+from config import config
 
-DB_PATH = _path / "database.json"
-
-db = TinyDB(DB_PATH, indent=4)
+db = TinyDB(config.DATABASE_PATH / "database.json", index=4)
